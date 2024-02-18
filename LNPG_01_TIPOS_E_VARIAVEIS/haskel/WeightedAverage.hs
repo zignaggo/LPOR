7f1f2d3a5a3e84265aacd8a1127cdb7757002bcb
zignaggo <jvz1@aluno.ifal.edu.br>
2024-02-18 15:23:47 -0300

    feature: add coments

--- LNPG_01_TIPOS_E_VARIAVEIS/haskel/WeightedAverage.hs
+++ LNPG_01_TIPOS_E_VARIAVEIS/haskel/WeightedAverage.hs
@@ -1,9 +1,10 @@
 module LNPG_01_TIPOS_E_VARIAVEIS.WeightedAverage where
 weightedAverage :: [[Float]] -> Float
 weightedAverage marks = sum [product x | x <- marks] / sum [x!!1 | x <- marks]
+-- [[mark, weight]] = [[10,2]]
 -- sum [x!!1 | x <- group] -- sum of weights
 -- sum [product x | x <- marks] -- sum of mark * weight
 main :: IO ()
 main = do
-    let marks = [[10, 1], [9, 1]]
+    let marks = [[10, 1], [9, 1], [8, 2]]
     print (weightedAverage marks)
