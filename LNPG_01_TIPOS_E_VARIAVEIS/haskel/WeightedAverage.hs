90f377ca41dbf7974b1210dce47305c4002bd87a
zignaggo <jvz1@aluno.ifal.edu.br>
2024-02-18 14:51:31 -0300

    feature: two versions of weightedAverage in haskell

--- LNPG_01_TIPOS_E_VARIAVEIS/haskel/WeightedAverage.hs
+++ LNPG_01_TIPOS_E_VARIAVEIS/haskel/WeightedAverage.hs
@@ -1,1 +1,24 @@
 module LNPG_01_TIPOS_E_VARIAVEIS.WeightedAverage where
+-- Notations
+-- (head: tail)
+-- v1 working
+-- sumWeights :: [[Float]] -> Float
+-- sumWeights weights = sum [x!!1 | x <- weights]
+-- multiplyMarks :: [[Float]] -> [Float]
+-- multiplyMarks marks = [product x | x <- marks]
+-- main :: IO ()
+-- main = do
+--     let marks = [[10, 2], [9, 2]]
+--     let weights = sumWeights marks
+--     let marksMultiplayed = multiplyMarks marks
+--     let result = sum marksMultiplayed  / weights
+--     print result
+weightedAverage :: [[Float]] -> Float
+weightedAverage marks = sum [product x | x <- marks] / sum [x!!1 | x <- marks]
+-- sum [x!!1 | x <- group] -- sum of weights
+-- sum [product x | x <- marks] -- product between mark and weight and sum of result
+main :: IO ()
+main = do
+    let marks = [[10, 1], [9, 1]]
+    let average = weightedAverage marks
+    print average
